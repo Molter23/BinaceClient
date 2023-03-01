4dd26944f456a0bd0bc1d61decc6fe66e61355e8
Maciek <maciek.cholewa.vip@gmail.com>
2023-03-01 17:17:22 +0100

    Addprod environement

--- src/BinanceClient/MarkerDataClient.py
+++ src/BinanceClient/MarkerDataClient.py
@@ -1,27 +1,39 @@
 import configparser
 import requests
 
+
 class Config:
 
-    def __init__(self):
+    def __init__(self) -> None:
         config = configparser.ConfigParser()
         config.read('API.ini')
         self.config = config
         
-    def get_url(self):
-        return self.config['TEST']['URL']
-        
+    def get_url(self, env: str) -> str:
+        if(env == 'TEST'):
+            return self.config['TEST']['URL']
+        elif(env =='PROD'):
+            return self.config['TEST']['URL']
+        else:
+            raise NameError('There is no URL defined for this environment')
 
+
+class AveragePrice:
+
+    def __init__(self, time_interval: int, price: float):
+        self.time_interval = time_interval
+        self.price = price
+    
 
 class MarkerDataClient:
 
-    def __init__(self) -> None:
+    def __init__(self, env: str) -> None:
         config = Config()
-        BASE_URL = config.get_url()
+        BASE_URL = config.get_url(env)
        
         self.endpoints = {'time': f'{BASE_URL}time',
-                          'exchange_info': f'{BASE_URL}exchangeInfo?symbol=BNBBTC',
-                          'depth' : f'{BASE_URL}depth?'
+                          'depth': f'{BASE_URL}depth?',
+                          'avg_price': f'{BASE_URL}avgPrice?' 
                          }
  
     def get_server_time(self) -> int:
@@ -31,14 +43,8 @@ class MarkerDataClient:
         else:
             raise ValueError("Unexcepted error while fetching data")
         
-    def get_exchange_info(self): # parse info 
-        r = requests.get(self.endpoints['exchange_info'])
-        if r.status_code == 200:
-            return r.json()
-        else:
-            raise ValueError("Unexcepted error while fetching data")
         
-    def get_order_book(self, symbol: str='BTCUSDT', limit: int=1):
+    def get_order_book(self, symbol: str='BTCUSDT', limit: int=1): # parse info
         query_string = f"{self.endpoints['depth']}symbol={symbol}&limit={str(limit)}"
         r = requests.get(query_string)
         if r.status_code == 200:
@@ -46,6 +52,11 @@ class MarkerDataClient:
         else:
             raise ValueError("Unexcepted error while fetching data")
 
-
-    
+    def get_average_price(self, symbol: str='BTCUSDT') -> AveragePrice:
+        query_string = f"{self.endpoints['avg_price']}symbol={symbol}"
+        r = requests.get(query_string)
+        if r.status_code == 200:
+            return AveragePrice(r.json()['mins'],  r.json()['price'])
+        else:
+            raise ValueError("Unexcepted error while fetching data")
 
--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from BinanceClient.MarkerDataClient import MarkerDataClient
 
 def main():
     if __name__ == "__main__":
-        Client = MarkerDataClient()
+        Client = MarkerDataClient('TEST')
         try: 
-            server_time = Client.get_server_time()
-            print(f"Server time: {server_time}")
+            obj =  Client.get_average_price()
+            print(f"Server time: {obj.price} {obj.time_interval}")
         except ValueError as err:
             print("Handling run-time error: ", err)
             
